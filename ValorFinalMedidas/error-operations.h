1302de2ce484ff5cccaa7b626cbc203be1a5beac
andrekorol <anrobits@yahoo.com.br>
2018-02-28 18:41:51 -0300

    Remove unused error-operations files.

--- ValorFinalMedidas/error-operations.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef __ERROR_OPERATIONS_H_INCLUDED__
-#define __ERROR_OPERATIONS_H_INCLUDED__
-
-long double inpm_round(long double measurement, unsigned long long decimals);
-
-int number(int * num);
-
-#endif
