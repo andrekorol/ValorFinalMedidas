4b1d3f2857ca8ec8cbd91c126aa5e0f27691ada7
andrekorol <anrobits@yahoo.com.br>
2018-03-01 18:38:35 -0300

    Finish both ErrorOperations::Functions::get_measurements_vectors and ErrorOperations::Functions::get_less_decimals implementation and optimization.

--- ErrorOperations/ErrorOperations.h
+++ ErrorOperations/ErrorOperations.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "stdafx.h"
+#include <vector>
+
 #ifdef ERROR_OPERATIONS_EXPORTS  
 #define ERROR_OPERATIONS_API __declspec(dllexport)   
 #else  
@@ -11,6 +14,8 @@ namespace ErrorOperations
 	class Functions
 	{
 	public:
-		long double inpm_round(long double measurement, unsigned long long decimals);
+		static ERROR_OPERATIONS_API std::tuple<std::vector<std::string>, std::vector<long double>> get_measurements_vectors();
+		static ERROR_OPERATIONS_API unsigned long long get_less_decimals(std::vector<std::string> str_vec);
+		static ERROR_OPERATIONS_API long double inpm_round(long double measurement, unsigned long long decimals);
 	};
 }
